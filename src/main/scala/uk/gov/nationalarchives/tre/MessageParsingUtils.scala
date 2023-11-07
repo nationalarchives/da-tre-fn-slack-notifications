17f64e9699e1096cdbf99b9a17e7db208994ad46
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-11-07 16:23:37 +0000

    Custom TREError decoding

--- src/main/scala/uk/gov/nationalarchives/tre/MessageParsingUtils.scala
+++ src/main/scala/uk/gov/nationalarchives/tre/MessageParsingUtils.scala
@@ -1,18 +1,27 @@
 package uk.gov.nationalarchives.tre
 
 import io.circe.generic.auto._
-import io.circe.{Decoder, parser}
+import io.circe.{Decoder, HCursor, parser}
 import uk.gov.nationalarchives.common.messages.{Producer, Properties}
 import uk.gov.nationalarchives.da.messages.bag.available.{BagAvailable, ConsignmentType}
 import uk.gov.nationalarchives.da.messages.courtdocumentpackage.available.{CourtDocumentPackageAvailable, Status => CDPAStatus}
 import uk.gov.nationalarchives.da.messages.request.courtdocument.parse.RequestCourtDocumentParse
-import uk.gov.nationalarchives.tre.messages.treerror.{TreError, Status => TEStatus}
+import uk.gov.nationalarchives.tre.messages.treerror.{Parameters, TreError, Status => TEStatus}
 
 object MessageParsingUtils {
   implicit val producerDecoder: Decoder[Producer.Value] = Decoder.decodeEnumeration(Producer)
   implicit val consignmentTypeDecoder: Decoder[ConsignmentType.Value] = Decoder.decodeEnumeration(ConsignmentType)
   implicit val courtDocumentPackageAvailableStatusDecoder: Decoder[CDPAStatus.Value] = Decoder.decodeEnumeration(CDPAStatus)
   implicit val treErrorStatusDecoder: Decoder[TEStatus.Value] = Decoder.decodeEnumeration(TEStatus)
+
+  implicit val decodeTreParameters: Decoder[Parameters] = (c: HCursor) => for {
+    status <- c.downField("status").as[uk.gov.nationalarchives.tre.messages.treerror.Status.Value]
+    originator <- c.downField("originator").as[Option[String]]
+    reference <- c.downField("reference").as[String]
+    errors <- c.downField("errors").as[Option[String]](Decoder.decodeOption(Decoder.decodeString))
+  } yield {
+    uk.gov.nationalarchives.tre.messages.treerror.Parameters(status, originator, reference, errors)
+  }
 
   def parseGenericMessage(message: String): GenericMessage =
     parser.decode[GenericMessage](message).fold(error => throw new RuntimeException(error), identity)
