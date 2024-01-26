cf29928a9fe1979acd585656460606ed59d2117f
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-01-26 11:52:12 +0000

    Update aws-lambda-java-runtime-interface-client to 2.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 object Dependencies {
-  lazy val lambdaRuntimeInterfaceClient = "com.amazonaws" % "aws-lambda-java-runtime-interface-client" % "2.3.3"
+  lazy val lambdaRuntimeInterfaceClient = "com.amazonaws" % "aws-lambda-java-runtime-interface-client" % "2.4.1"
 }
